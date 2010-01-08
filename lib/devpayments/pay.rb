4cd8994e34f2902514596aabbce67c4656543eed
Patrick Collison <patrick@duo.local>
2010-01-08 01:52:55 -0300

    fix bindings

--- lib/devpayments/pay.rb
+++ lib/devpayments/pay.rb
@@ -7,46 +7,81 @@ module DevPayments
   class Client
     DEVPAY_API = 'https://api.devpayments.com/api'
     DEVPAY_PAGE_ROOT = 'https://devpayments.com/pay'
+    
+    def requires!(hash, *params)
+      params.each do |param| 
+        if param.is_a?(Array)
+          raise ArgumentError.new("Missing required parameter: #{param.first}") unless hash.has_key?(param.first) 
 
-    def initialize(token, key)
-      @token = token
+          valid_options = param[1..-1]
+          raise ArgumentError.new("Parameter: #{param.first} must be one of #{valid_options.to_sentence(:words_connector => 'or')}") unless valid_options.include?(hash[param.first])
+        else
+          raise ArgumentError.new("Missing required parameter: #{param}") unless hash.has_key?(param) 
+        end
+      end
+    end    
+
+    def initialize(key)
       @key = key
     end
 
-    def prepare_charge(amt, extra={})
-      req({
-        :amount => amt,
-        :currency => 'usd',
-        :url => '',
-        :extra => JSON.dump(extra),
+    def prepare(opts)
+      requires!(opts, :amount)
+      
+      opts = {
+        # defaults you can over-ride...
+        :currency => 'usd'
+      }.merge(opts).merge({
+        # and defaults you can't...
         :method => 'prepare_charge'
       })
+      
+      if opts[:extra]      
+        opts[:extra] = JSON.dump(opts[:extra])
+      end
+            
+      r = req(opts)
+      
+      OpenStruct.new(r)
     end
 
-    def retrieve_charge(c)
+    def retrieve(opts)
+      requires!(opts, :charge)
+      
       r = req({
-        :charge => c,
+        :charge => opts[:charge],
         :method => 'retrieve_charge'
       })
 
       r['extra'] = JSON.load(r['extra']) if(r['extra']) 
-      r
+      
+      OpenStruct.new(r)
     end
-
-    def execute_charge(c, ccdets)
-      req({
-        :charge => c,
-        :card => ccdets,
+    
+    def execute(opts)
+      requires!(opts, :charge, :card)
+      
+      r = req(opts.merge({
         :method => 'execute_charge'
-      })
+      }))
+      
+      OpenStruct.new(r)
+    end
+    
+    def credit(opts)
+      requires!(opts, :charge)
+      
+      r = req(opts.merge({
+        :method => 'credit_charge'
+      }))
+      
+      OpenStruct.new(r)
     end
 
     private
     def req(params)
       params = params.merge({
-        :key => @key,
-        :token => @token,
-        :sig => ''
+        :key => @key
       })
 
       d = RestClient.post(DEVPAY_API, params)
@@ -64,7 +99,7 @@ module DevPayments
 
   class Error < RuntimeError; end
 
-  def self.client(token, key)
-    Client.new(token, key)
+  def self.client(key)
+    Client.new(key)
   end
 end
