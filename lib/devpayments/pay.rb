fcdf9b7c726bc30025ba191f94bb31ee44d7a590
John <john@johnmacbook.local>
2010-01-08 02:38:23 -0200

    twiddling

--- lib/devpayments/pay.rb
+++ lib/devpayments/pay.rb
@@ -16,7 +16,7 @@ module DevPayments
       req({
         :amount => amt,
         :currency => 'usd',
-        :url => '',
+        :url => extra[:url] ? extra[:url] : '',
         :extra => JSON.dump(extra),
         :method => 'prepare_charge'
       })
