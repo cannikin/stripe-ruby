9523f8f7083d6f45e62915f771f817a11e2a55fe
John Collison <john@collison.ie>
2011-01-10 01:11:28 +0000

    bump version

--- lib/stripe.rb
+++ lib/stripe.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 class Stripe
-  VERSION = '1.3.4'
+  VERSION = '1.3.5'
 end
 
 require 'stripe/pay'
